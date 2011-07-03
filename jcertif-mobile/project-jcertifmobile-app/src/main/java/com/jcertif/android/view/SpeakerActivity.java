81ead55b0b987b768dd203260d717843cf7b6589
Rossi Oddet <rossi.oddet@gmail.com>
2011-07-03 00:09:36 +0000

    [VIEW] Modification de l'écran détail d'un speaker pour se rapprocher de la cible. Reste à faire : compléter la description avec les présentations des speakers

--- jcertif-mobile/project-jcertifmobile-app/src/main/java/com/jcertif/android/view/SpeakerActivity.java
+++ jcertif-mobile/project-jcertifmobile-app/src/main/java/com/jcertif/android/view/SpeakerActivity.java
@@ -17,42 +17,70 @@ import com.jcertif.android.model.Speaker;
 
 /**
  * Display details of a selected speaker
- * @author mouhamed_diouf
  *
+ * @author mouhamed_diouf
  */
 public class SpeakerActivity extends Activity {
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.speaker_detail);
-		int speakerId = getIntent().getIntExtra("speakerId", -1);
-		ImageView i11 = (ImageView) findViewById(R.id.speakerImg);
-		TextView speakerFirstName = (TextView) findViewById(R.id.speakerFirstName);
-		TextView speakerLastName = (TextView) findViewById(R.id.speakerLastName);
-		TextView speakerCompagny = (TextView) findViewById(R.id.speakerCompany);
-		TextView speakerBio = (TextView) findViewById(R.id.speakerBio);
+    public static final int BIO_PART1_MAX_LENGTH = 100;
 
-		DatabaseHelper dbHelper = new DatabaseHelper(getBaseContext());
-		Dao<Speaker, Integer> speakerDao;
-		try {
-			speakerDao = dbHelper.getSpeakerDao();
-			Speaker speaker = speakerDao.queryForId(speakerId);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.speaker_detail);
+        int speakerId = getIntent().getIntExtra("speakerId", -1);
+        ImageView i11 = (ImageView) findViewById(R.id.speakerImg);
 
-			
-			Bitmap speakerBitmap = BitmapFactory.decodeFile(Environment.getExternalStorageDirectory().getAbsolutePath() + "/"
-					+ speaker.urlPhoto);
-			i11.setImageBitmap(speakerBitmap);
-			speakerFirstName.setText(speaker.firstName);
-			speakerLastName.setText(speaker.lastName); 
-			speakerCompagny.setText(speaker.company);
-			speakerBio.setText(speaker.bio);
+        TextView speakerBioPart1 = (TextView) findViewById(R.id.speakerBioPart1);
+        TextView speakerBioPart2 = (TextView) findViewById(R.id.speakerBioPart2);
 
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			Log.e("SpeakerDisplayActivity", e.getMessage());
-		}
+        DatabaseHelper dbHelper = new DatabaseHelper(getBaseContext());
+        Dao<Speaker, Integer> speakerDao;
+        try {
+            speakerDao = dbHelper.getSpeakerDao();
+            Speaker speaker = speakerDao.queryForId(speakerId);
 
-	}
+            // Define header title
+            TextView headerTitle = (TextView) findViewById(R.id.header_title);
+            headerTitle.setText(speaker.firstName + " " + speaker.lastName + " (" + speaker.company + ")");
+
+            TextView speakerName = (TextView) findViewById(R.id.speakerName);
+            speakerName.setText(speaker.firstName + " " + speaker.lastName);
+
+            Bitmap speakerBitmap = BitmapFactory.decodeFile(Environment.getExternalStorageDirectory().getAbsolutePath() + "/"
+                    + speaker.urlPhoto);
+            i11.setImageBitmap(speakerBitmap);
+
+            String[] splittedBio = splitBio(speaker.bio);
+            speakerBioPart1.setText(splittedBio[0]);
+            speakerBioPart2.setText(splittedBio[1]);
+
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            Log.e("SpeakerDisplayActivity", e.getMessage());
+        }
+
+    }
+
+    /**
+     * Spliting bio.
+     * @param bio  a bio
+     * @return String array (2 elements) with two parts
+     */
+    private String[] splitBio(String bio) {
+        StringBuilder part1 = new StringBuilder();
+        StringBuilder part2 = new StringBuilder();
+        String[] spaceSplitBio = bio.split(" ");
+
+        for (String word : spaceSplitBio) {
+                 if(part1.length() <= BIO_PART1_MAX_LENGTH){
+                     part1.append(word + " ");
+                 } else {
+                     part2.append(word + " ");
+                 }
+        }
+
+        return new String[]{part1.toString(), part2.toString()};
+    }
 
 }
